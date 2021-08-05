1c1e3dafc0cd103540b74e71110fffcbfc5f6ed3
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-05 18:11:57 +0530

    Terraform Output

--- 02-Variables/vars.tf
+++ 02-Variables/vars.tf
@@ -6,6 +6,12 @@ variable "sample2" {
   default = "Sathish"
 }
 
-output Output {
-  value = var.sample2
+output "Output" {
+  value = var.sample
 }
+
+output "Output2" {
+  value = "${var.sample} - From Terraform"
+  //Variable if it is combined within some other string then we have to use ${} interpolation for variable accessing
+}
+
