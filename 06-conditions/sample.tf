24ae118121dc19f95b61a3f0fdfb796e105f0b2a
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-06 21:31:34 +0530

    Terraform

---
+++ 06-conditions/sample.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "sample" {
+  count                  = 0
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = var.TYPE == null ? "t3.micro" : var.TYPE
+  //vpc_security_group_ids = [var.SG_ID]
+
+  tags                   = {
+    Name                 = "Example"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "TYPE" {
+  default = null
+}
