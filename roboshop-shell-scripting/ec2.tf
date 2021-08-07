a7491c1c42845d18d703d85a4ef034b8ffd367e3
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-07 16:38:10 +0530

    Terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -11,33 +11,6 @@ resource "aws_spot_instance_request" "cheap_worker" {
   }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-}
-
-
-resource "null_resource" "run-shell-scripting" {
-  count                     = local.LENGTH
-  provisioner "remote-exec" {
-    connection {
-      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
-      user                  = "centos"
-      password              = "DevOps321"
-    }
-
-    inline = [
-      "cd /home/centos",
-      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-      "cd shell-scripting/roboshop",
-      "git pull",
-      "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
-  }
-}
-
 locals {
   LENGTH    = length(var.COMPONENTS)
 }
