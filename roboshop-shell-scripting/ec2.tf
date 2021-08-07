093f719f1e8845f8507d3a94891364057bc59511
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-07 16:24:20 +0530

    Terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                       = "ami-074df373d6bafa625"
   spot_price                = "0.0031"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-0d00b6b80c9e9b60c"]
+  vpc_security_group_ids    = ["sg-083a944fa2575b4b6"]
   wait_for_fulfillment      = true
   spot_type                 = "persistent"
   tags                      = {
