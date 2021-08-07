3342433a0ee90723655a31fb4ac4849d01cb9056
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-07 17:13:47 +0530

    Terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -29,7 +29,7 @@ resource "null_resource" "run-shell-scripting" {
     }
     inline = [
       "cd /home/centos",
-      "git clone https://github.com/ChavitiSathish/shellscripting.git",
+      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
       "cd shell-scripting/roboshop",
       "git pull",
       "sudo make ${element(var.COMPONENTS, count.index)}"
