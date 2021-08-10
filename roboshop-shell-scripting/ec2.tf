ee6d324ee68c16b8c1067132559579529683bf32
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-10 08:54:15 +0530

    Terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_route53_record" "records" {
   count                     = local.LENGTH
   name                      = element(var.COMPONENTS, count.index)
   type                      = "A"
-  zone_id                   = "Z05578043NRAM9S9QZLUY"
+  zone_id                   = "Z07786171B1287DQL7RM6"
   ttl                       = 300
   records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
 }
