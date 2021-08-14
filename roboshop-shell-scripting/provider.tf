f8d910232211806cbb99cfeeb13bdcfa7b802438
Chaviti Sathish <chavitisathish@gmail.com>
2021-08-14 11:39:20 +0530

    Terraform

--- roboshop-shell-scripting/provider.tf
+++ roboshop-shell-scripting/provider.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket               = "chtfbucket"
-    key                  = "roboshop/shell-scripting/terraform.tfstate"
-    region               = "us-east-1"
-    dynamodb_table       = "terraform"
+    bucket            = "chtfbucket"
+    key               = "roboshop/shell-scripting/terraform.tfstate"
+    region            = "us-east-1"
+    dynamodb_table    = "terraform"
   }
 }
